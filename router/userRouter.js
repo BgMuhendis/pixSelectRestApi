25a04754f5a8c4532b0a26a8dcf7a8f14d542036
Muhammet Hadi KAMAT <muhammedhhadikamat@gmail.com>
2021-05-02 15:13:27 +0300

    taskSon

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const userController = require("../controller/userController");
 
-
 router.post("/register",  userController.userRegister);
 router.post("/login", userController.userSignIn);
 
